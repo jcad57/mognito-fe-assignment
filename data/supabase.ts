f22469c213e21dffd4baa7c568ad4d0653384d3d
Jon <jcadeichmann@gmail.com>
2025-04-30 12:14:40 -0500

    update comments for ENV file

--- data/supabase.ts
+++ data/supabase.ts
@@ -1,9 +1,9 @@
 // Normally I would not include .env files in the repository,
 // but for the sake of this example, I will include it.
+// RLS is enabled inside Supabase to prevent unauthorized access to the database.
 
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-// RLS is enabled inside Supabase, so we use the anon key
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
