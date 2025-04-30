32602e36f0baa9145fc7fb099e074994cbca80c5
Jon <jcadeichmann@gmail.com>
2025-04-30 12:10:22 -0500

    include env file

--- data/supabase.ts
+++ data/supabase.ts
@@ -1,3 +1,6 @@
+// Normally I would not include .env files in the repository,
+// but for the sake of this example, I will include it.
+
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 // RLS is enabled inside Supabase, so we use the anon key
